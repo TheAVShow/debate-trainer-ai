db2d0252c1e211a1a27b6633e76dcd414492119a
Cody Meyer <cmmeyer2019@gmail.com>
2025-06-03 17:58:12 -0500

    Fixed .env with correct Firebase base64

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-// test.js
 require('dotenv').config();
 const base64String = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
 const decodedString = Buffer.from(base64String, 'base64').toString('utf-8');
 try {
   const json = JSON.parse(decodedString);
-  console.log("Parsed successfully:", json);
+  console.log("Parsed successfully:", json.project_id);
 } catch (error) {
   console.error("Error:", error.message);
 }
